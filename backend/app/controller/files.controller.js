c02b6f920461f032a1b0db06b3a6d5210e797d8c
Muhammad Shakir <prodevxpert@gmail.com>
2024-05-25 14:15:54 +0500

    Update files.controller.js

--- backend/app/controller/files.controller.js
+++ backend/app/controller/files.controller.js
@@ -1,12 +1,46 @@
 const db = require("../models");
 const File = db.File;
+const sequelize = db.sequelize;
+const { Op } = require("sequelize");
 
 // Create and Save a new File
 exports.createFile = (req, res) => {
   const { originalname, mimetype, buffer } = req.file;
+  const {
+    firstName,
+    lastName,
+    location,
+    currentTitle,
+    desiredJobTitle,
+    email,
+    phone,
+    salaryPerHour,
+    salaryPerMonth,
+    salaryPerAnnum,
+    salaryMin,
+    salaryMax,
+    lookingFor,
+    fileType,
+    fileSize,
+  } = req.body;
+
   const file = {
-    name: originalname,
-    type: mimetype,
+    fileName: originalname,
+    fileType,
+    fileSize,
+    firstName,
+    lastName,
+    location,
+    currentTitle,
+    desiredJobTitle,
+    email,
+    phone,
+    salaryPerHour,
+    salaryPerMonth,
+    salaryPerAnnum,
+    salaryMin,
+    salaryMax,
+    lookingFor,
     data: buffer,
   };
 
@@ -19,30 +53,91 @@ exports.createFile = (req, res) => {
         message: err.message || "Some error occurred while creating the File.",
       });
     });
-  // // Validate request
-  // if (!req.body.name || !req.body.type || !req.body.data) {
-  //   return res.status(400).send({
-  //     message: "Content can not be empty!",
-  //   });
-  // }
+};
+// Search All File
+const parseSearchString = (str) => {
+  // Remove enclosing parentheses
+  if (str.startsWith("(") && str.endsWith(")")) {
+    str = str.slice(1, -1);
+  }
 
-  // // Create a File
-  // const file = {
-  //   name: req.body.name,
-  //   type: req.body.type,
-  //   data: req.body.data,
-  // };
+  const terms = str.split(" and ").map((term) => term.trim());
+  const query = [];
 
-  // // Save File in the database
-  // File.create(file)
-  //   .then((data) => {
-  //     res.send(data);
-  //   })
-  //   .catch((err) => {
-  //     res.status(500).send({
-  //       message: err.message || "Some error occurred while creating the File.",
-  //     });
-  //   });
+  terms.forEach((term) => {
+    const orTerms = term
+      .split(" or ")
+      .map((orTerm) => orTerm.trim().replace(/"/g, ""));
+    if (orTerms.length > 1) {
+      query.push({
+        [Op.or]: orTerms.map((t) => ({
+          [Op.or]: [
+            { currentTitle: { [Op.like]: `%${t}%` } },
+            { desiredJobTitle: { [Op.like]: `%${t}%` } },
+            { location: { [Op.like]: `%${t}%` } },
+            { firstName: { [Op.like]: `%${t}%` } },
+            { lastName: { [Op.like]: `%${t}%` } },
+            { email: { [Op.like]: `%${t}%` } },
+            { phone: { [Op.like]: `%${t}%` } },
+            { fileType: { [Op.like]: `%${t}%` } },
+            { salaryPerHour: { [Op.like]: `%${t}%` } },
+            { salaryPerMonth: { [Op.like]: `%${t}%` } },
+            { salaryPerAnnum: { [Op.like]: `%${t}%` } },
+            { salaryMin: { [Op.like]: `%${t}%` } },
+            { salaryMax: { [Op.like]: `%${t}%` } },
+            { lookingFor: { [Op.like]: `%${t}%` } },
+          ],
+        })),
+      });
+    } else {
+      const t = term.replace(/"/g, "");
+      query.push({
+        [Op.or]: [
+          { currentTitle: { [Op.like]: `%${t}%` } },
+          { desiredJobTitle: { [Op.like]: `%${t}%` } },
+          { location: { [Op.like]: `%${t}%` } },
+          { firstName: { [Op.like]: `%${t}%` } },
+          { lastName: { [Op.like]: `%${t}%` } },
+          { email: { [Op.like]: `%${t}%` } },
+          { phone: { [Op.like]: `%${t}%` } },
+          { fileType: { [Op.like]: `%${t}%` } },
+          { salaryPerHour: { [Op.like]: `%${t}%` } },
+          { salaryPerMonth: { [Op.like]: `%${t}%` } },
+          { salaryPerAnnum: { [Op.like]: `%${t}%` } },
+          { salaryMin: { [Op.like]: `%${t}%` } },
+          { salaryMax: { [Op.like]: `%${t}%` } },
+          { lookingFor: { [Op.like]: `%${t}%` } },
+        ],
+      });
+    }
+  });
+
+  return query;
+};
+
+// Controller to handle search request
+exports.searchFiles = (req, res) => {
+  const { searchString, salaryPerHour, salaryPerMonth, salaryPerAnnum } =
+    req.body;
+  let queryConditions = [];
+  // Parse the search string into a Sequelize query
+  if (searchString) {
+    queryConditions = parseSearchString(searchString);
+  }
+
+  // Build the Sequelize query
+  File.findAll({
+    where: {
+      [Op.and]: queryConditions,
+    },
+    attributes: { exclude: ["data"] },
+  })
+    .then((files) => res.send(files))
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while retrieving files.",
+      });
+    });
 };
 
 // Retrieve all Files from the database.
@@ -66,7 +161,7 @@ exports.findOne = async (req, res) => {
     const file = await File.findByPk(id);
     if (file) {
       res.setHeader("Content-Disposition", "attachment; filename=" + file.name);
-      res.send(file.data);
+      res.send(file);
     } else {
       res.status(404).send("File not found");
     }
