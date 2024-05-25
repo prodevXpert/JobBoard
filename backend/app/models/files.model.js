9af45ece6f351e21bc437ecd406d431d09492dae
Muhammad Shakir <prodevxpert@gmail.com>
2024-05-25 14:15:15 +0500

    Update files.model.js

--- backend/app/models/files.model.js
+++ backend/app/models/files.model.js
@@ -1,18 +1,61 @@
 module.exports = (sequelize, Sequelize) => {
   const File = sequelize.define("file", {
-    name: {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    firstName: {
       type: Sequelize.STRING,
     },
-    type: {
+    lastName: {
       type: Sequelize.STRING,
     },
-    // size: {
-    //   type: Sequelize.STRING,
-    // },
+    location: {
+      type: Sequelize.STRING,
+    },
+    currentTitle: {
+      type: Sequelize.STRING,
+    },
+    desiredJobTitle: {
+      type: Sequelize.STRING,
+    },
+    email: {
+      type: Sequelize.STRING,
+    },
+    phone: {
+      type: Sequelize.STRING,
+    },
+    salaryPerHour: {
+      type: Sequelize.STRING,
+    },
+    salaryPerMonth: {
+      type: Sequelize.STRING,
+    },
+    salaryPerAnnum: {
+      type: Sequelize.STRING,
+    },
+    salaryMin: {
+      type: Sequelize.STRING,
+    },
+    salaryMax: {
+      type: Sequelize.STRING,
+    },
+    lookingFor: {
+      type: Sequelize.STRING,
+    },
+    fileName: {
+      type: Sequelize.STRING,
+    },
+    fileType: {
+      type: Sequelize.STRING,
+    },
+    fileSize: {
+      type: Sequelize.STRING,
+    },
     data: {
       type: Sequelize.BLOB("long"),
     },
   });
   return File;
 };
- 
